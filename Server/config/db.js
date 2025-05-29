7d513271d115fb378c4db8523dceaf96d3f1b9cb
PathumLD <pathumlk10158@gmail.com>
2025-05-29 12:10:01 +0530

    Db changed

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,11 +2,23 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected...');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connected to DB');
+    });
+    
+    mongoose.connection.on('error', (err) => {
+      console.error('Mongoose connection error:', err);
+    });
+    
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose disconnected');
+    });
   } catch (err) {
     console.error('Database connection error:', err.message);
     process.exit(1);
